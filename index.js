1118db68a4eaa66d271709648362425f2624ad16
John Gratton <jgratton@gmail.com>
2018-03-24 15:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var total = total();
+    var value = total();
     setCart([]);
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${value}, which will be charged to the card ${cardNumber}.`;
   }
 }
 

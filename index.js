b705184b7f2f35e1b4ed0e556517df16ea0be6e6
John Gratton <jgratton@gmail.com>
2018-03-24 15:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === -1) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
 }
 
 /* OPEN TEST AREA
